2806465a6323fc12f0fca32e6c7a20156e0f89ff
rupam905 <rupamdutta905.com>
2025-07-19 22:07:01 +0530

    header & signin signup added

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,9 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "AI Career Coach",
@@ -13,10 +14,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+    }}>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${inter.className} `}>
+        <body className={`${inter.className} `}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -27,7 +29,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             {/* footer */}
             <footer className="bg-muted/50 py-12">
-              <div>
+              <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made with Love</p>
               </div>
             </footer>
--- components/header.jsx
+++ components/header.jsx
@@ -24,7 +24,7 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
-const header = () => {
+const Header = () => {
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -103,4 +103,4 @@ const header = () => {
   );
 };
 
-export default header;
+export default Header;
